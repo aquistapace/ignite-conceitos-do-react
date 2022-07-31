3e1f34266974e6b07738e6935cfa974f0a2a2280
aquistapace <carol.aquist@gmail.com>
2022-07-30 21:27:18 -0300

    Undo test

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -50,7 +50,7 @@ export function TaskList() {
         <div className="input-group">
           <input
             type="text"
-            placeholder="Adicionar novo todo test"
+            placeholder="Adicionar novo todo"
             onChange={e => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
